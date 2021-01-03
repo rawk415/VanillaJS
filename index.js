e399847f29c975d87d2fb291659ff2eba621b2ed
Kwon Hoechang <rawk415@gmail.com>
2021-01-03 06:49:22 +0000

    Study about Array[] & Object{}

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('this is JS, vanilla JS');
+const kwonInfo = {
+  name : "kwon",
+  age : 23,
+  girlfriend : true,
+  favoriteFood: ["apple", "meat", "steak"],
+  favoriteMovie: [
+    {
+      name: "parasite",
+      genre: "thriller"
+    },
+    {
+      name: "No Country for Oldman",
+      genre: "thriller"
+    }
+  ]
+}
+
+console.log(kwonInfo.favoriteFood[2]);
+console.log(kwonInfo.favoriteMovie[0].name);
+console.log(kwonInfo.age);
