c32a0699c704c23ec07ff66d1d8e6b25892d0f94
Kwon Hoechang <rawk415@gmail.com>
2021-01-04 14:24:25 +0000

    document == HTML innerHTML == change Text

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
-const kwonInfo = {
-  name : "kwon",
-  age : 23,
-  girlfriend : true,
-  favoriteFood: ["apple", "meat", "steak"],
-  favoriteMovie: [
-    {
-      name: "parasite",
-      genre: "thriller"
-    },
-    {
-      name: "No Country for Oldman",
-      genre: "thriller"
-    }
-  ]
-}
+const title = document.getElementById("title");
 
-console.log(kwonInfo.favoriteFood[2]);
-console.log(kwonInfo.favoriteMovie[0].name);
-console.log(kwonInfo.age);
+console.log(title);
 
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function(a, b) {
-    return a - b;
-  },
-  multiple: function(a, b) {
-    return a * b;
-  },
-  divide: function(a, b) {
-    return a / b;
-  },
-  time: function(a, b) {
-    let c = a;
-
-    if(b == 0) {
-      return 0;
-    } else if(b<0) {
-      return false;
-    }
-
-    for(let i=1; i < b; i++) {
-    c = c * a;
-    }
-    return c;
-    
-  }
-}
-console.log(calculator.time(2, 10));
-console.log(`5+4=${calculator.plus(5,4)}, 5/2=${calculator.divide(5, 2)}`);
+title.innerHTML("Hi, from JS");
